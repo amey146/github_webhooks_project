eb27d7b4db2c0b2bca6a2be1ba91928b189a41e0
amey146 <ameygaikwad1464@gmail.com>
2025-02-13 09:20:45 +0530

    initial commit

---
+++ ProgramA.java
@@ -0,0 +1,9 @@
+public class ProgramA {
+    public static void main(String[] args) {
+        int A = 10;
+        int B = 20;
+
+        int sum = A + B;
+        System.out.println("The sum of A and B is: " + sum);
+    }
+}
